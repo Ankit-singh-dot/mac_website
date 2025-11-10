8a56274d11c3a769c17e917e2e7c69ad6afd0cbf
Ankit <ankitrajdihara123@gmail.com>
2025-11-11 02:42:32 +0530

    store_zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ProductReview from "./components/ProductReview";
 
 function App() {
-
-
-  return(<div>
-    <main>
-      <Navbar/>
-      <Hero/>
-    </main>
-  </div>)
+  return (
+    <div>
+      <main>
+        <Navbar />
+        <Hero />
+        <ProductReview />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
